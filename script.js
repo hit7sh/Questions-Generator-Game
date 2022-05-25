a16a82c1ef5579cc0dca9f38e69678e96718852e
Hitesh Kumar Saini <hit7sh@gmail.com>
2022-05-25 17:02:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function temp() {
 	window.alert("You Clicked Home")
 }
-
-var modal=getElementById('id_1');
-			window.onclick=function(event){
-				if(event.target==modal){
-					modal.style.display="none";
-				}
-			}
+// This is a comment by me.
+var modal = getElementById('id_1');
+window.onclick = function (event) {
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
+}
